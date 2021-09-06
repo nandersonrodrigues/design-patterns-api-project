71a5b8e94c334f526fab2558e59355f690e39ea6
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-09-06 16:36:36 -0300

    start

---
+++ src/main/java/com/digitalinnovation/one/designpatternsapiproject/DesignPatternsApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.designpatternsapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digitalinnovation/one/designpatternsapiproject/DesignPatternsApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.designpatternsapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
